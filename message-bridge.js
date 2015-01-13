e6104ccde0d2333005f5f05356a0b69939e3215f
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-13 17:56:31 +1100

    cleanup

--- message-bridge.js
+++ message-bridge.js
@@ -1,5 +1,3 @@
-var extend = require('cog/extend');
-
 module.exports = function(opts) {
   var port = chrome.runtime.connect(chrome.runtime.id);
 
@@ -9,30 +7,12 @@ module.exports = function(opts) {
 
 
   function handleMessage(evt) {
-    var data = evt && evt.data;
-
     if (evt.source != window) {
       return;
     }
 
-    console.log('bridge got message: ', evt);
-    port.postMessage(data);
-
-//     // if we have data and a target extension defined, then send it through
-//     if (data && data.target) {
-//       chrome.runtime.sendMessage(data, function(response) {
-//         window.postMessage(response, '*');
-//       });
-//     }
+    port.postMessage(evt.data);
   }
-
-//   chrome.runtime.onMessage.addListener(function(data, sender) {
-//     if (sender.tab) {
-//       return;
-//     }
-
-//     window.postMessage(extend(data, { src: 'extension' }), '*');
-//   });
 
   window.addEventListener('message', handleMessage);
 };
