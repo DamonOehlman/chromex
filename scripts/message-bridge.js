f330af8778d124c35bbb8a9d173195de7d3a5e93
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-14 17:03:27 +1100

    Move message-bridge into a standalone scripts directory

--- scripts/message-bridge.js
+++ scripts/message-bridge.js
@@ -1,18 +1,20 @@
-module.exports = function(opts) {
+(function() {
   var port = chrome.runtime.connect(chrome.runtime.id);
-
   port.onMessage.addListener(function(msg) {
     window.postMessage(msg, '*');
   });
-
 
   function handleMessage(evt) {
     if (evt.source != window) {
       return;
     }
 
-    port.postMessage(evt.data);
+    // if this is a tagged request from the chromex client then passthrough
+    if (evt.data && evt.data.requestId) {
+      port.postMessage(evt.data);
+    }
   }
 
+  console.log('chromex message bridge initialized');
   window.addEventListener('message', handleMessage);
-};
+})();
